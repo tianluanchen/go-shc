c2024e1abee8ede0816cf51b9729c33d05acaf20
tianluanchen <zxl-life@outlook.com>
2024-02-29 19:03:47 +0800

    Update workflows

--- build.sh
+++ build.sh
@@ -1,36 +1,57 @@
 #!/usr/bin/env bash
-
 go mod tidy && go mod download
 
-build_with_os_arch() {
-    export GOOS=$1
-    export GOARCH=$2
-    output=$3
-    if [ -z "$output" ]; then
-        output="$(basename $(go list -m))_#OS#_#ARCH#"
+build_with_osarch() {
+    osarch=$1
+    goos=$(echo "$osarch" | cut -d '/' -f1)
+    if [ -z "$goos" ]; then
+        goos=$(go env GOOS)
     fi
-    echo "building for $(go env GOOS)/$(go env GOARCH)"
+    goarch=$(echo "$osarch" | cut -d '/' -f2)
+    if [ -z "$goarch" ] || [ "$goarch" = "$osarch" ]; then
+        goarch=$(go env GOARCH)
+    fi
+    export GOOS=$goos
+    export GOARCH=$goarch
+    output=$2
+    # mod_basename=$(basename "$(go list -m)")
+    mod_basename=$(basename "$(head -n 1 go.mod | cut -f 2 -d " ")")
+    if [ -d "$output" ]; then
+        if [ "${output: -1}" = "/" ]; then
+            output="${output}#MOD#_#OS#_#ARCH#"
+        else
+            output="${output}/#MOD#_#OS#_#ARCH#"
+        fi
+    fi
+    if [ -z "$output" ]; then
+        output="#MOD#_#OS#_#ARCH#"
+    fi
     suffix=""
-    if [ "$GOOS" = "windows" ]; then
+    if [ "$GOOS" = "windows" ] && [ "${output: -4}" != ".exe" ]; then
         suffix=".exe"
     fi
-    output=$(echo "${output}${suffix}" | sed "s/#OS#/${GOOS}/g" | sed "s/#ARCH#/${GOARCH}/g")
-    go build -ldflags "-s -w " \
+    output=$(echo "${output}${suffix}" | sed "s/#MOD#/${mod_basename}/g" | sed "s/#OS#/${GOOS}/g" | sed "s/#ARCH#/${GOARCH}/g")
+    echo "building for $(go env GOOS)/$(go env GOARCH)  ==>  ${output}"
+    go build -ldflags "-s -w $LDFLAGS" \
         -gcflags="all=-trimpath=${PWD}" \
         -asmflags="all=-trimpath=${PWD}" \
         -o "${output}"
 }
-name="./bin/$(basename $(go list -m))_#OS#_#ARCH#"
 
-if [ -d "bin" ]; then
-    rm bin/*
+output_dir="./bin/"
+
+if [ -d $output_dir ]; then
+    rm -rf $output_dir
 fi
 
-build_with_os_arch linux amd64 "$name" &
-build_with_os_arch linux arm64 "$name" &
-build_with_os_arch windows amd64 "$name" &
-build_with_os_arch windows arm64 "$name" &
-build_with_os_arch darwin amd64 "$name" &
-build_with_os_arch darwin arm64 "$name" &
+mkdir -p $output_dir
+
+LDFLAGS="-X github.com/tianluanchen/go-shc/shc.Version=0.0.$(date -u +"%Y%m%d%H%M%S")"
+
+targets=("linux/amd64" "linux/arm64" "windows/arm64" "windows/amd64" "darwin/amd64" "darwin/arm64")
+
+for osarch in "${targets[@]}"; do
+    build_with_osarch "$osarch" "$output_dir" &
+done
 wait
-echo "fininshed"
+echo "build completed"
